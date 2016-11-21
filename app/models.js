af6da92d4a343a368ee931780a8cddb43a777848
Tyler Benton <tjbenton21@gmail.com>
2016-11-21 12:44:44 -0500

    reworked `models.js`
    This update removes several functions that aren't needed and aslo removes the `async` from functions that don't actually need it. It simplifies the overall purpose of the file by using existing helpers, and reworks other functions that were to verbose.

--- app/models.js
+++ app/models.js
@@ -1,7 +1,7 @@
 import yaml from 'yamljs';
+import { map } from 'async-array-methods';
+import fs from 'fs-extra-promisify';
 import path from 'path';
-import Chance from 'chance';
-const chance = new Chance();
 import documents from './documents';
 import * as utils from './utils';
 import objectPath from 'object-path';
@@ -15,180 +15,116 @@ let model_documents_count = {}; // global variable to hold the number of documen
 let settings; // global variable to hold the available options / settings
 
 // pre run setup / handle settings
-export function prepare(options) {
-  return new Promise((resolve, reject) => {
-    settings = options;
+export async function prepare(options) {
+  settings = options;
+  if (!options.models) return;
 
-    list(options)
-      .then(filter)
-      .then(load)
-      .then(validate)
-      .then(parse)
-      .then(resolveDependencies)
-      .then(() => setDocumentCounts(options))
-      .then(resolve)
-      .catch((err) => {
-        reject(err);
-      });
-  });
-}
+  // get list of files
+  let files = await map(options.models.split(/\s*,\s*/), (str) => !!path.extname(str) ? str : utils.findFiles(str));
+  // flattens the array of files and filter files for valid input formats: csv, json, cson, yaml and zip
+  files = to.flatten(files).filter((file) => !!file && /\.ya?ml$/i.test(file));
 
-// gets the available model yaml files from the current working directory
-function list(options) {
-  return new Promise((resolve, reject) => {
-    // console.log('models.list');
-    try {
-      utils.isDirectory(options.models)
-        .then(utils.readDirectory)
-        .then((files) => {
-          resolve(files);
-        })
-        .catch(() => {
-          resolve(options.models.split(','));
-        });
-    } catch (e) {
-      reject(e);
-    }
-  });
-}
+  if (!files.length) throw new Error('No valid model files found.');
 
-// filter files for valid models
-async function filter(files) {
-  // console.log('models.filter');
-  files = files.filter((file) => {
-    return file.match(/\.ya?ml$/i);
-  });
-  if (!files.length) {
-    throw new Error('No valid model files found.');
-  }
-  return files;
-}
-
-// loop over all of the found yaml files and load them
-async function load(files) {
-  // console.log('models.load');
-  let tmp = [];
   model_count = files.length;
-  files.forEach((file) => {
-    tmp.push(loadYamlFile(file));
-  });
-  return await Promise.all(tmp);
+
+  await map(files, parse);
+
+  resolveDependencies();
+  setDocumentCounts(options);
+  return model_documents_count;
 }
 
-// load and conver a yaml file to a json object
-function loadYamlFile(file) {
-  return new Promise((resolve, reject) => {
-    // console.log('models.loadYamlFile');
-    yaml.load(path.resolve(file), (result) => {
-      if (result) {
-        models[result.name || file] = result; // add the parsed model to the global object
-        resolve();
-      } else {
-        reject(new Error(`Invalid YAML file: ${file}`));
-      }
-    });
-  });
-}
+async function parse(file) {
+  // read yaml file and convert it to json
+  const model = yaml.parse(to.string(await fs.readFile(path.resolve(file))));
 
-// validate the model
-async function validate() {
-  // console.log('models.validate');
-  for (let model in models) {
-    if (!models[model].name) {
-      throw new Error(`The model ${model} must have a "name" property.`);
-    }
-    if (!models[model].type) {
-      throw new Error(`The model ${model} must have a "type" property.`);
-    }
-    if (!models[model].key) {
-      throw new Error(`The model ${model} must have a "key" property.`);
-    }
+  if (!model.name) {
+    model.name = file;
   }
-}
 
-// parse each of the model properties for a build functions
-async function parse() {
-  // console.log('models.parse');
-  try {
-    let parsed = [];
-    for (let model in models) { // loop over each model
-      if (models.hasOwnProperty(model)) {
-        parsed.push(parseModel(model));
-      }
-    }
-    return await Promise.all(parsed);
-  } catch (e) {
-    throw e;
+  // validate the model
+  if (!model.type) {
+    throw new Error(`The model ${model.name} must have a "type" property.`);
   }
-}
+  if (!model.key) {
+    throw new Error(`The model ${model.name} must have a "key" property.`);
+  }
 
-async function parseModel(model) {
+
+  // add the parsed model to the global object
+  // should always have a model name
+  models[model.name] = model;
+
   // console.log('models.parseModel');
-  await parseModelFunctions(model);
-  await parseModelReferences(model);
-  await parseModelTypes(model);
-  await parseModelDefaults(model);
+  parseModelFunctions(model.name);
+  parseModelReferences(model.name);
+  parseModelTypes(model.name);
+  parseModelDefaults(model.name);
 }
 
 // searches the model for any of the pre / post run and build functions and generates them
-async function parseModelFunctions(model) {
+function parseModelFunctions(name) {
+  const model = models[name];
   // console.log('models.parseModelFunctions');
-  let results = utils.objectSearch(models[model], /((pre|post)_run)|(pre_|post_)?build$/);
+  const results = utils.objectSearch(model, /((pre|post)_run)|(pre_|post_)?build$/);
   results.forEach((function_path) => {
     try {
       objectPath.set(
-        models[model],
+        model,
         function_path,
         /* eslint-disable no-new-func */
         new Function(
           'documents', 'globals', 'inputs', 'faker', 'chance', 'document_index',
-          objectPath.get(models[model], function_path)
+          objectPath.get(model, function_path)
         )
         /* eslint-enable no-new-func */
       );
     } catch (e) {
-      throw new Error(`Function Error in model "${models[model].name}", for property: ${function_path}, Reason: ${e.message}`);
+      throw new Error(`Function Error in model '${model.name}', for property: ${function_path}, Reason: ${e.message}`);
     }
   });
 }
 
 // searches the model for any '$ref' values that are pointing to definitions, sub_models, etc. and copies the reference to the schema
-async function parseModelReferences(model) {
+function parseModelReferences(name) {
+  const model = models[name];
   // console.log('models.parseModelReferences');
-  let pattern = /\.(schema|items).\$ref$/;
-  let results = utils.objectSearch(models[model], pattern);
+  const pattern = /\.(schema|items).\$ref$/;
+  const results = utils.objectSearch(model, pattern);
   results.sort(); // sort the array so definitions come first before properties, this allows definitions to have definitions
   results.forEach((reference_path) => {
-    let property_path = reference_path.replace(pattern, '') + (reference_path.indexOf('.items.') !== -1 ? '.items' : '');
-    let property = objectPath.get(models[model], property_path);
-    let defined_path = objectPath.get(models[model], reference_path).replace(/^#\//, '').replace('/', '.');
-    // property = objectMerge({}, property, objectPath.get(models[model], defined_path));
-    property = to.extend(to.clone(property), objectPath.get(models[model], defined_path));
-    objectPath.set(models[model], property_path, property);
+    const property_path = reference_path.replace(pattern, '') + (reference_path.indexOf('.items.') !== -1 ? '.items' : '');
+    let property = objectPath.get(model, property_path);
+    const defined_path = objectPath.get(model, reference_path).replace(/^#\//, '').replace('/', '.');
+    property = to.extend(to.clone(property), objectPath.get(model, defined_path));
+    objectPath.set(model, property_path, property);
   });
 }
 
 // searches the model for any properties or items and makes sure the defaults exist
-async function parseModelTypes(model) {
+function parseModelTypes(name) {
+  const model = models[name];
   // console.log('models.parseModel_properties');
-  let results = utils.objectSearch(models[model], /.*properties\.[^.]+(\.items)?$/);
+  const results = utils.objectSearch(model, /.*properties\.[^.]+(\.items)?$/);
   results.forEach((type_path) => {
-    let property = objectPath.get(models[model], type_path);
+    const property = objectPath.get(model, type_path);
     // make sure there is a type property set
     if (!property.hasOwnProperty('type')) {
       property.type = 'undefined';
-      objectPath.set(models[model], type_path, property);
+      objectPath.set(model, type_path, property);
     }
   });
 }
 
 // sets any model defaults that are not defined
-async function parseModelDefaults(model) {
+function parseModelDefaults(name) {
+  const model = models[name];
   // console.log('models.parseModelDefaults');
   // find properties or items that do not have a data block and assign it
-  let results = utils.objectSearch(models[model], /^(.*properties\.[^.]+)$/);
+  let results = utils.objectSearch(model, /^(.*properties\.[^.]+)$/);
   results.forEach((data_path) => {
-    let property = objectPath.get(models[model], data_path);
+    let property = objectPath.get(model, data_path);
     // if the property is an array that has an items block but not a data block, default it
     if (property.type === 'array') {
       if (property.items && !property.items.data) {
@@ -197,29 +133,25 @@ async function parseModelDefaults(model) {
     } else if (!property.data) {
       property.data = {};
     }
-    objectPath.set(models[model], data_path, property);
+    objectPath.set(model, data_path, property);
   });
   // find any data property at the root or that is a child of items and make sure it has the defaults for min, max, fixed
-  if (!models[model].data) { // if a data property wasn't set define it
-    models[model].data = {};
+  if (!model.data) { // if a data property wasn't set define it
+    models[name].data = model.data = {};
   }
-  results = utils.objectSearch(models[model], /^(.*properties\.[^.]+\.items\.data|(data))$/);
-  const data_defaults = {
-    min: 0,
-    max: 0,
-    fixed: 0
-  };
+  results = utils.objectSearch(model, /^(.*properties\.[^.]+\.items\.data|(data))$/);
+  const data_defaults = { min: 0, max: 0, fixed: 0 };
   results.forEach((data_path) => {
     objectPath.set(
-      models[model],
+      model,
       data_path,
-      to.extend(to.clone(data_defaults), objectPath.get(models[model], data_path))
+      to.extend(to.clone(data_defaults), objectPath.get(model, data_path))
     );
   });
 }
 
 // resolve the dependencies and establish the order the models should be parsed in
-async function resolveDependencies() {
+function resolveDependencies() {
   // console.log('models.resolveDependencies');
   let counter = 0;
   // continue looping until all dependencies are resolve or we have looped (model_count * 5) times at which point
@@ -292,7 +224,7 @@ async function setDocumentCounts() {
     if (settings.number) {
       number = parseInt(settings.number);
     } else {
-      number = current_model.data.fixed || chance.integer({ min: current_model.data.min, max: current_model.data.max }) || 1;
+      number = current_model.data.fixed || to.random(current_model.data.min, current_model.data.max) || 1;
     }
     model_documents_count[v] = number;
   });
@@ -301,12 +233,12 @@ async function setDocumentCounts() {
 
 // handles generation of data for each model
 export async function generate(options) {
-  // console.log('models.generate');
-  for (let i = 0; i < model_order.length; i++) { // loop over each model and execute in order of dependency
+  // this has to run in order because other documents might depend on prior documents.
+  for (let order of model_order) {
     await documents(// eslint-disable-line babel/no-await-in-loop
-      models[model_order[i]],
-      model_documents_count[models[model_order[i]].name],
-      typeof settings.number !== 'undefined' && parseInt(settings.number) > 0 && settings.exclude.indexOf(models[model_order[i]].name) === -1,
+      models[order],
+      model_documents_count[models[order].name],
+      settings.number != null && parseInt(settings.number) > 0 && settings.exclude.indexOf(models[order].name) === -1,
       options
     ); // eslint-disable-line babel/no-await-in-loop
   }
@@ -314,5 +246,5 @@ export async function generate(options) {
 
 // handles generation of data for each model
 export function getModelNames() {
-  return Object.keys(models);
+  return to.keys(models);
 }
