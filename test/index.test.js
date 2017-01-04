ad4c5e20e5a11d6cd641fd4d210b845c539fc8a7
Tyler Benton <tjbenton21@gmail.com>
2017-01-04 16:39:16 -0500

    Finished up Fakeit.generate

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,13 @@
 import proxyquire from 'proxyquire';
 import default_options from '../dist/output/default-options';
 import to from 'to-js';
+import globby from 'globby';
+import fs from 'fs-extra-promisify';
 import { stdout } from 'test-console';
-import { join as p } from 'path';
+import path, { join as p } from 'path';
 import { stripColor } from 'chalk';
 import ava from 'ava-spec';
-import { without } from 'lodash';
+import AdmZip from 'adm-zip';
 
 /* istanbul ignore next */
 const Document = require('../dist/documents.js').default;
@@ -39,6 +41,10 @@ const Fakeit = proxyquire('../dist/index.js', {
 
 const test = ava.group('fakeit');
 const fakeit_root = p(__dirname, 'fixtures', 'models');
+const folder_root = p(__dirname, 'fixtures', 'fakeit-folder-test');
+const zip_root = p(__dirname, 'fixtures', 'fakeit-zip-test');
+
+test.before(() => Promise.all([ fs.remove(folder_root), fs.remove(zip_root) ]));
 
 /* istanbul ignore next */
 const models = require('./utils').models({
@@ -50,15 +56,6 @@ const models = require('./utils').models({
     return model.replace(/models(.*)\.yaml/g, 'validation$1.data.js');
   }
 });
-
-const done = [
-  p('contacts', 'models', 'contacts.yaml'),
-  p('music', 'models', 'countries.yaml')
-];
-
-function filterDone() {
-  return without(models.files, ...done);
-}
 
 test.beforeEach((t) => {
   t.context.fakeit = new Fakeit({
@@ -100,40 +97,61 @@ generate.serial.group('console', models(async (t, model) => {
   const inspect = stdout.inspect();
   await t.context.fakeit.generate(model, t.context.defaults);
   inspect.restore();
+  const actual = to.object(stripColor(inspect.output[0].trim()))[0];
 
-  return to.object(stripColor(inspect.output[0].trim()))[0];
-}, null, filterDone()));
+  return actual;
+}));
 
 generate.group('return', models(async (t, model) => {
   t.context.defaults.output = 'return';
-  // var shit = to.object(()[0])
   let actual = await t.context.fakeit.generate(model, t.context.defaults);
   // get the first item in the list of the tests
   // and convert it to an object
   actual = to.object(actual[0]);
   // get the first item in the array to test
   return actual[0];
-}, null, filterDone()));
+}));
 
 generate.group('folder', models(async (t, model) => {
-  t.is(typeof model, 'string');
-  t.pass();
-}, null, models.files));
+  const root = p(folder_root, model.replace(new RegExp(path.sep, 'g'), '-').replace('.yaml', ''));
+  t.context.defaults.output = root;
+  await t.context.fakeit.generate(model, t.context.defaults);
+  const files = await globby(p(root, '**', '*'));
+  t.is(files.length, 1);
+  return fs.readJson(files[0]);
+}));
 
 generate.group('zip', models(async (t, model) => {
-  t.is(typeof model, 'string');
-  t.pass();
-}, null, models.files));
+  const root = p(zip_root, model.replace(new RegExp(path.sep, 'g'), '-').replace('.yaml', ''));
+  t.context.defaults.output = root;
+  t.context.defaults.archive = 'archive.zip';
+  await t.context.fakeit.generate(model, t.context.defaults);
+  const files = await globby(p(root, '**', '*'));
+  t.is(files.length, 1);
+  t.is(path.extname(files[0]), '.zip');
+  const zip = new AdmZip(files[0]);
+  t.is(zip.getEntries().length, 1);
+  const entry_file = zip.getEntries()[0].name;
+  t.is(path.extname(entry_file), '.json');
+  // read the entry_file from the zip file and convert it to an object from a json string
+  return to.object(await zip.readAsText(entry_file));
+}));
 
-generate.group('couchbase', models(async (t, model) => {
-  t.is(typeof model, 'string');
-  t.pass();
-}, null, models.files));
-
-generate.group('sync-gateway', models(async (t, model) => {
-  t.is(typeof model, 'string');
-  t.pass();
-}, null, models.files));
+// couchbase and sync-gateway are too difficult to test in this way
+// since we have to use the mock equivalents of some of their inner functions
+// so they are not tested here, but they're functionality is tested else where.
+// generate.group('couchbase', models(async (t, model) => {
+//   t.is(typeof model, 'string');
+//   t.pass();
+// }, null, models.files));
+//
+// generate.group('sync-gateway', models(async (t, model) => {
+//   t.is(typeof model, 'string');
+//   t.pass();
+// }, null, models.files));
 
 // log all the schema keys that still need to be done
-test.after(models.todo);
+test.after.always(() => {
+  models.todo();
+  return Promise.all([ fs.remove(folder_root), fs.remove(zip_root) ]);
+});
