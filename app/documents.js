fe3d27b5220d139308b0dcdc5f6fab6c964444b7
AlexRamey <alex.b.ramey@gmail.com>
2017-11-14 09:16:32 -0500

    1. only push unique document keys to documents[model.name]

--- app/documents.js
+++ app/documents.js
@@ -163,6 +163,7 @@ export class Document extends Base {
 
     spinner.start();
     const delay = (duration) => new Promise((resolve) => setTimeout(resolve, duration));
+    var built_docs_map = {};
     await pool(model.data.count, async (i) => { // loop over each model and execute in order of dependency
       // don't anymore if the program should exit
       if (exit) return;
@@ -172,7 +173,11 @@ export class Document extends Base {
       const doc = await this.buildDocument(model, i);
       update();
       await delay(0);
-      this.documents[model.name].push(doc);
+      // only push the document if the key is not already taken
+      if (built_docs_map[doc.__key] === undefined) {
+        built_docs_map[doc.__key] = doc;
+        this.documents[model.name].push(doc);
+      }
     }, this.options.spinners ? 75 : 1000)
       .catch((err) => {
         spinner.fail(err);
