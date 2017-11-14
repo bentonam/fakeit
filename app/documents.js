d3a69232279e113beb95fd6b7f41250640b154c2
AlexRamey <alex.ramey@willowtreeapps.com>
2017-11-14 11:21:01 -0500

    addressed PR comments

--- app/documents.js
+++ app/documents.js
@@ -163,7 +163,7 @@ export class Document extends Base {
 
     spinner.start();
     const delay = (duration) => new Promise((resolve) => setTimeout(resolve, duration));
-    var built_docs_map = {};
+    const built_docs_map = {};
     await pool(model.data.count, async (i) => { // loop over each model and execute in order of dependency
       // don't anymore if the program should exit
       if (exit) return;
@@ -176,10 +176,13 @@ export class Document extends Base {
       // only push the document if the key is not already taken
       /* eslint no-underscore-dangle: ["error", { "allow": ["__name", "__key"] }] */
       if (!built_docs_map[doc.__key]) {
-        built_docs_map[doc.__key] = doc;
+        built_docs_map[doc.__key] = true;
         this.documents[model.name].push(doc);
       }
     }, this.options.spinners ? 75 : 1000)
+      .then(() => {
+        built_docs_map = {};
+      })
       .catch((err) => {
         spinner.fail(err);
       });
