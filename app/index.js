8d268b692ea9cf6dd4cb4dbdabcbf16e38958ea3
Aaron Benton <bentonam@gmail.com>
2018-01-14 16:56:01 -0500

    Added initial support for streams

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ import Base from './base';
 import to from 'to-js';
 import { uniqueId } from 'lodash';
 import Documents from './documents';
+import DocumentsStream from './documents-stream';
 import { success } from 'log-symbols';
 
 /// @name Fakeit
@@ -48,23 +49,48 @@ export default class Fakeit extends Base {
     const label = uniqueId('fakeit');
     this.time(label);
     const model = new Models(this.options);
+
     const output = new Output(this.options, output_options);
     output.prepare();
 
     await model.registerModels(models);
+    // calculate the total # of dependencies, if it is 0 and we're using Couchbase,
+    // we can leverage streams to output the data.
+    let total_dependants = 0;
+    model.models.forEach((value) => {
+      total_dependants += value.dependants.length;
+    });
     await output.preparing;
 
-    const documents = new Documents(this.options, this.documents, this.globals, model.inputs);
-    delete model.inputs;
-    let result = documents.build(model.models);
-    documents.on('data', (data) => output.output(data));
-    result = await result;
-    await output.finalize();
-    const time = this.timeEnd(label);
-    if (this.options.verbose) {
-      console.log(`${success} Finished generating ${documents.total} documents in ${time}`);
+    // @todo add additional check for cli argument, as using streams as it could introduce unexpected behavior
+    // and some features might not be available
+    if (output_options.output === 'couchbase' && !total_dependants) { // we're outtputting to couchbase and there aren't any dependants use streams
+      const documents = new DocumentsStream(
+        this.options,
+        this.globals,
+        model.inputs,
+        output,
+      );
+      delete model.inputs;
+      let result = await documents.build(model.models);
+      await output.finalize();
+      const time = this.timeEnd(label);
+      if (this.options.verbose) {
+        console.log(`${success} Finished generating ${documents.total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} documents in ${time}`);
+      }
+      return result;
+    } else {
+      const documents = new Documents(this.options, this.documents, this.globals, model.inputs);
+      delete model.inputs;
+      let result = documents.build(model.models);
+      documents.on('data', (data) => output.output(data));
+      result = await result;
+      await output.finalize();
+      const time = this.timeEnd(label);
+      if (this.options.verbose) {
+        console.log(`${success} Finished generating ${documents.total} documents in ${time}`);
+      }
+      return result;
     }
-
-    return result;
   }
 }
